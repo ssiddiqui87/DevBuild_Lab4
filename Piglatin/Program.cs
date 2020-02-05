9ef7531f8cb33a067adf2e078c9f2cb266be7617
Sameer Siddiqui <sameersiddiqui@quickenloans.com>
2020-02-05 13:34:34 -0500

    commit for Piglatin bonus lab

--- Piglatin/Program.cs
+++ Piglatin/Program.cs
@@ -7,8 +7,7 @@ namespace Piglatin
     {
         static void Main(string[] args)
         {
-            //Get input from user
-            
+            //Get input from user           
             Console.Write("Enter a word: ");
             string input = Console.ReadLine().ToLower();
             string vowel = "aeiou";
@@ -19,10 +18,12 @@ namespace Piglatin
            
             foreach (string word in splitArray)
             {
+                //If the word starts with a vowel, it will call the Vowel method
                 if (vowel.Contains(word[0]))
                 {
                     Console.Write(Vowel(word)+ " ");
                 }
+                //Otherwise it calls the Consonant method
                 else
                 {
                     Console.Write(Consonant(word) + " ");
@@ -31,60 +32,39 @@ namespace Piglatin
 
         }
 
+        //Just returns the input word with "way" concatenated
         static string Vowel(string input)
         {
             return (input + "way");
         }
 
+
         static string Consonant(string input)
         {
             string consonant = "bcdfghjklmnpqrstvwxyz";
-
             Queue<char> consonantQueue = new Queue<char>();
             string output = "";
             int ctr = 0;
 
+            //This will check each letter and if it is a consonant, it will put it in constantQueue until it gets to a vowel
             while (consonant.Contains(input[ctr]))
             {
                 consonantQueue.Enqueue(input[ctr]);
                 ctr++;
             }
 
+           //Once a vowel is enountered, this loop will put the rest of the letters in "output"
             for (int i = 0; i < (input.Length - ctr); i++)
             {
                 output += input[i + ctr];
             }
 
+            //Dequeue from queue back onto output
             output += consonantQueue.Dequeue();
 
-
+            //return output word with "ay" concatenated at the end.
             return (output + "ay");
         }
 
     }
 }
-//for (int i = 0; i < input.Length; i++)
-//{
-//    if (consonant.Contains(input[i]))
-//    {
-//        consonantQueue.Enqueue(input[i]);
-//    }
-//    else if (vowel.Contains(input[i]))
-//    {
-//        ctr = i;
-//        for (int j = 0; j < (input.Length - i); j++)
-//        {
-//            restOfWord.Enqueue(input[ctr]);
-//            ctr++;
-//        }
-//        break;
-//    }
-//}
-//for (int i = 0; restOfWord.Count > 0; i++)
-//{
-//    output += restOfWord.Dequeue();
-//}
-//for (int i = 0; consonantQueue.Count > 0; i++)
-//{
-//    output += consonantQueue.Dequeue();
-//}
